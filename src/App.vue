497490a1efb0f920a486fede8d197200c1c9aa8d
nickmorano <49941706+nickmorano@users.noreply.github.com>
2022-02-25 16:37:23 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <v-content>
+      <NavBar />
+      <router-view />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar'
+
+export default {
+  name: 'App',
+  components: {
+    NavBar
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
